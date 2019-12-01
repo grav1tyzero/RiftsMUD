c47dca4a99ff1bd28a762d00cf3aa6ee785a3ae6
James Peckham <jdpeckham@gmail.com>
2019-11-30 23:21:22 -0600

    more funky time issues.live with 30 days a month

--- mudlib/adm/include/clock.h
+++ mudlib/adm/include/clock.h
@@ -1,11 +1,14 @@
 #define THE_BEGINNING 720561600
 #define NOW(x) x-THE_BEGINNING
 
-#define SECOND 1
+//don't use second because it doesn't really exist.
+//#define SECOND 1
 #define MINUTE 8
 #define HOUR 480
 #define DAY 11520
 #define WEEK 80640
+// used for the day of the month (CURRENT_DATE)
+#define MONTH_DAY 139776
 #define MONTH 4193280
 #define YEAR 50319360
 
@@ -13,9 +16,13 @@
 #define CURRENT_MONTH(x) ((NOW(x))%YEAR)/MONTH
 #define CURRENT_WEEK(x) ((NOW(x))%MONTH)/WEEK
 #define CURRENT_DAY(x) ((NOW(x))%WEEK)/DAY
-#define CURRENT_DATE(x) (1+((NOW(x))%MONTH)/DAY)
+
+#define CURRENT_DATE(x) (1+((NOW(x))%MONTH)/MONTH_DAY)
+//hour of the day
 #define CURRENT_HOUR(x) ((NOW(x))%DAY)/HOUR
+//minute of the hour
 #define CURRENT_MINUTE(x) ((NOW(x))%HOUR)/MINUTE
+// second of the hour
 #define CURRENT_SECOND(x) ((NOW(x))%MINUTE)*MINUTE
 
 #define LONGEST_DAY 101
